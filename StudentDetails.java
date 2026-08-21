0bfd1063a21ee7d382b065999670ea2c991109e3
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-21 16:40:07 +0530

    Add StudentDetails class to capture student info

---
+++ StudentDetails.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class StudentDetails
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Name:");
+        String name=sc.next();
+        System.out.println("Enter Your Age:");
+        int age=sc.nextInt();
+        System.out.println("Enter Your Previous Education Degree:");
+        String degree=sc.next();
+        System.out.println("Enter Your Percentage:");
+        int per=sc.nextInt();
+        System.out.println("Thank You!");
+
+    }
+}
