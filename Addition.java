175bbc2212650867c50b43a67d95bf767d062974
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-21 16:41:38 +0530

    Addition Program

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Addition
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int no1=sc.nextInt();
+        System.out.println("Enter Second Number:");
+        int no2=sc.nextInt();
+        int add=no1+no2;
+        System.out.println("Addition:"+add);
+    }
+}
