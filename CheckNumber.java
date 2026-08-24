48a80e9732f68e5b4c360ad9864e6983352e53b9
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-24 16:28:33 +0530

    java condition statement practice

---
+++ CheckNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class CheckNumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int num=sc.nextInt();
+        if (num>0)
+        {
+            System.out.println(num+" is Positive");
+        }
+        else if (num<0)
+        {
+            System.out.println(num+" is Negative");
+        }
+        else
+        {
+            System.out.println("Number is Zero");
+        }
+    }
+}
