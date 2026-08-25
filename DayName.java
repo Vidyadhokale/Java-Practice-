eb540093fbb5b513f3cf7b2fab6ee972e967e85e
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-25 16:13:49 +0530

    java Switch Statement Practice

---
+++ DayName.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class DayName
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome To Day Name");
+        System.out.println("1,2,3,4,5,6,7");
+        System.out.println("Enter Your Choice:");
+        int ch=0;
+        ch=sc.nextInt();
+        switch(ch)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+
+
+        }
+    }
+}
