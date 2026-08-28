c26b081239158152d07662b8fea3dd47a1da29cc
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-28 16:22:40 +0530

    java:- Array Largest Number Find Practice

---
+++ ArrLargestNo.java
@@ -0,0 +1,15 @@
+public class ArrLargestNo
+{
+    public static void main(String args[])
+    {
+        int arr[]={10,20,30,40,50};
+        int largest=arr[0];
+        for(int i=0; i< arr.length;i++)
+        {
+            if (arr[i]>largest)
+            {
+               largest=arr[i];
+            }
+        }System.out.println("Largest Array Element:"+largest);
+    }
+}
