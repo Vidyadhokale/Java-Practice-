428e82eceb3fef5370726ca250da374c09650e35
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-28 16:24:02 +0530

    java:-Array Find  Smallest Number
    Implement a program to find the smallest number in an array.

---
+++ ArrSmallestNo.java
@@ -0,0 +1,15 @@
+public class ArrSmallestNo
+{
+    public static void main(String args[])
+    {
+        int arr[]={10,20,30,40,50};
+        int smallest=arr[0];
+        for(int i=0; i< arr.length;i++)
+        {
+            if (arr[i]<smallest)
+            {
+                smallest=arr[i];
+            }
+        }System.out.println("Smallest Array Element:"+smallest);
+    }
+}
