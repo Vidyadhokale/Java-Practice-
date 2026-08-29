3ac23745c1dfe79cc374fb6628fa06cd08f9a36a
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-29 20:33:58 +0530

    Java String Method Practice

---
+++ PrintCharacters.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PrintCharacters
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String:");
+        String str=sc.next();
+        for(int i=0;i<str.length();i++)
+        {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
