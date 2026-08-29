a8403cfdbe16dd44201f7e7a149102d76fb8b262
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-29 20:35:00 +0530

    Java String Method Practice

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ReverseString
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String:");
+        String str=sc.next();
+        System.out.println("Reverse String:");
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            System.out.print(str.charAt(i));
+        }
+
+    }
+}
