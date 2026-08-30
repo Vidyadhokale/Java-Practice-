3aa47b28ceb5125e3f510793c6f4fb7068b7ac91
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-30 16:36:14 +0530

    Java: 2D Array Practice

---
+++ Print2DArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Print2DArray
+{
+    public static void main(String args[])
+    {
+        int arr[][]=new int[3][3];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Matrix Element:");
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("The Matrix is:");
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                System.out.print(arr[i][j]+ " ");
+            }System.out.println();
+        }
+    }
+}
