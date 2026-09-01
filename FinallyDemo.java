adb1dad590ef4dacf3fb5e35514e4e677ce7ee86
Vidyadhokale <vidyadhokale4@gmail.com>
2026-09-01 14:41:29 +0530

    Add Java exception handling practice

---
+++ FinallyDemo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class FinallyDemo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int num1=sc.nextInt();
+        System.out.println("Enter Second Number:");
+        int num2=sc.nextInt();
+        try
+        {
+            System.out.println("Division:"+(num1/num2));
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Can not Divided by Zero");
+        }
+        finally
+        {
+            System.out.println("Finally Block Executed");
+        }
+    }
+}
