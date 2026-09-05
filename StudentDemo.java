3a291f44c576e675cfbee89c6ff20dae17838e1e
Vidyadhokale <vidyadhokale4@gmail.com>
2026-09-05 19:45:35 +0530

    In Java Class and Object Practice

---
+++ StudentDemo.java
@@ -0,0 +1,16 @@
+class Student1
+{
+    String name ;
+    int age;
+}
+public class StudentDemo
+{
+    public static void main(String args[])
+    {
+        Student1 st=new Student1();
+        st.name="vidya";
+        st.age=21;
+        System.out.println("Name:"+st.name);
+        System.out.println("Age:"+st.age);
+    }
+}
