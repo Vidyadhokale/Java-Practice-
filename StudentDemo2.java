1970b351d613b7197e6f3a3c7b8222f2255dec9e
Vidyadhokale <vidyadhokale4@gmail.com>
2026-09-05 19:47:02 +0530

    In Java Topic Class and Object Practice
    Fix constructor method name to match class name and correct output for age.

---
+++ StudentDemo2.java
@@ -0,0 +1,16 @@
+class Student2
+{
+    public void Student(String name,int age)
+    {
+        System.out.println("Name:"+name);
+        System.out.println("Name:"+age);
+    }
+}
+public class StudentDemo2
+{
+    public static void main(String args[])
+    {
+        Student2 st=new Student2();
+        st.Student("vidya",21);
+    }
+}
