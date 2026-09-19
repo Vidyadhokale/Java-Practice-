e9dc37542b6d4ecab13d5775aa0dd93d47554c49
Vidyadhokale <vidyadhokale4@gmail.com>
2026-09-19 23:05:12 +0530

    Add files via upload

---
+++ MethodOverride.java
@@ -0,0 +1,25 @@
+class Animal
+{
+    void sound()
+    {
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal
+{
+    @Override 
+    void sound()
+    {
+        System.out.println("Dog barks");
+    }
+}
+public class MethodOverride 
+{
+    public static void main(String args[])
+    {
+        Dog d=new Dog();
+        d.sound();
+    }
+}
+
+
